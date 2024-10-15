3d52b6738c97b62f883bf1d200eed16315d877c9
quondem <86314992+quondem@users.noreply.github.com>
2024-10-16 02:39:16 +0300

    adding dist

---
+++ assets/default-BbuSLbEb.js
@@ -0,0 +1,1 @@
+import{c as o,w as e,V as r,r as n,o as s,a as t,b as _}from"./index-BrMcwe4n.js";const f={__name:"default",setup(c){return(l,u)=>{const a=n("router-view");return s(),o(r,null,{default:e(()=>[t(_,null,{default:e(()=>[t(a)]),_:1})]),_:1})}}};export{f as default};
